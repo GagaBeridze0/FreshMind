5efc7cbd6c4232d04df5335c8b5e1a47d1b39666
gagaberidze0 <gagaberidze00@gmail.com>
2023-11-01 15:32:34 +0400

    Fresh

--- Assets/Script/quiz/questions.js
+++ Assets/Script/quiz/questions.js
@@ -2,71 +2,57 @@
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
+    question: "დნობის განსაკუთრებულად მაღალი ტემპით გამოირჩევა",
     answer: "Hyper Text Markup Language",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "აღმოსავლეთ კავკასიონი",
+      "დასავლეთ კავკასიონი",
+      "ცენტრალური კავკასიონი",
+      "ყველა"
     ]
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
+    question: "გლობალური დათბობის ერთ-ერთი მნიშვნელოვანი გამომწვევი მიზეზია",
     answer: "Cascading Style Sheet",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "მყინვარების დნობა",
+      "ტყის გაჩეხვა",
+      "ტემპერატურის საგრძნობლად ცვლილება",
+      "სამივე"
     ]
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
+    question: "ბუნებრივი კატასტროფების ზრდის ხელშემწყობი ფაქტორია",
     answer: "Hypertext Preprocessor",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+      "გლობალური დათბობის შედეგად კლიმატის საგრძნობლად ცვლილება",
+      "ტემპერატურის გადაჭარბდებული ზრდა",
+      "ტყის ხანძრები",
+      "მყინვარების დნობა"
     ]
   },
     {
     numb: 4,
-    question: "What does SQL stand for?",
+    question: "რა გახდა შოვის ტრაგედიის მიზეზი?",
     answer: "Structured Query Language",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+      "გლაციალური ღვარცოფი",
+      "ვულკანის ამოფრქვევა",
+      "ზვავი",
+      "წყალმოვარდნა"
     ]
   },
     {
     numb: 5,
-    question: "What does XML stand for?",
+    question: "როგორი ტიპის კატასტროფაა ტექნოგენური?",
     answer: "eXtensible Markup Language",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "გარემოდაცვითი",
+      "გლობალური",
+      "ადამიანის საქმიანობით განპირობებული",
+      "არცერთი"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
